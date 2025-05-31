536399ef806e3fb7eebdf35f80740c7f52ab3578
Adomako N <adomako.nyarko4@gmail.com>
2025-05-30 22:45:22 -0400

    finished num_guess

--- num_guess_game/num_guess.py
+++ num_guess_game/num_guess.py
@@ -1,1 +1,20 @@
-# Prompt: "Guess the number between 1 and 100:". I
+# Prompt: "Guess a number between 1 and 100:". If number not inputted, return "Enter a valid number". If number higher, return "Too high", If number lower, return, "too low".
+import random
+
+num = ((random.randint(1, 100)))
+while True:
+    try:
+        prompt = int(input("Guess a number between 1 and 100: "))
+        if prompt > num:
+            print("Too high")
+        elif prompt < num:
+            print("Too low")
+        else:
+            print("Congrats, you guessed the number")
+            exit()
+    except ValueError:
+        print("Enter a valid number")
+
+
+
+
