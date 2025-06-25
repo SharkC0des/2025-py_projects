5a9e8aef42d539462a72df570e3dd3c897a867ee
Adomako N <adomako.nyarko4@gmail.com>
2025-06-24 23:19:06 -0400

    Sloppy coded but everything orks as it should except for my w key

--- timedMath/timed_Math.py
+++ timedMath/timed_Math.py
@@ -21,7 +21,7 @@ def addition(n1, n2):
     return f"{n1} + {n2} = "
 
 def subtraction(n1, n2):
-    return f"{n2} - {n1} = "
+    return f"{n1} - {n2} = "
 
 def multiply(n1, n2):
     return f"{n1} * {n2} = "
@@ -29,85 +29,97 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return f"{n1} / {n2} = "
 def gameplay():
-    count = 0 # Keeps Score
-    while True:
-        num1 = 0
-        num2 = 0
-        #functions
-        add = addition(num1, num2)
-        sub = subtraction(num1, num2)
-        times = multiply(num1, num2)
-        div = divide(num1, num2)
-
-        op_list = [div]
-        choose = random.choice(op_list)
-
-        # case for multiplication
-
-        if choose == add:
-            while True:
-                num1 = random.randint(1, 100)
-                num2 = random.randint(1, 100)
-                add = addition(num1, num2)
-                add_sum = num1 + num2
-                if add_sum > 100:
-                    continue
-                break
-            equation = int(input(add))
-            if add_sum == equation:
-                print("Correct")
-                count += 1
-                continue
-            print("incorrect")
-            break
-
-        elif choose == sub:
-            num1 = random.randint(1, 100)
-            num2 = random.randint(1, 100)
-            sub = subtraction(num2, num1)
-            if num2 > num1:
-                sub = subtraction(num1, num2)
-            equation = int(input(sub))
-            diff = num2 - num1
-            if diff == equation:
-                print("Correct")
-                count += 1
-                continue
-            print("Incorrect")
-            print(diff)
-            break
-        elif choose == times:
-            num1 = random.randint(1, 12)
-            num2 = random.randint(1, 12)
+        count = 0 # Keeps Score
+        while True:
+            num1 = 0
+            num2 = 0
+            #functions
+            add = addition(num1, num2)
+            sub = subtraction(num1, num2)
             times = multiply(num1, num2)
-            product = num1 * num2
-            equation = int(input(times))
-            if product == equation:
-                print("Correct")
-                count += 1
-                continue
-            print("Incorrect")
-            break
-        elif choose == div:
-            while True:
-                num1 = random.randint(1, 144)
-                num2 = random.randint(1, 12)
-                if num1 > 12:
-                    div = divide(num1, num2)
-                    quotient = num1 / num2
-                    if num2 % num1 != 0 and quotient < 12:
+            div = divide(num1, num2)
+
+            op_list = [sub]
+            choose = random.choice(op_list)
+
+            # case for multiplication
+
+
+            if choose == add:
+                while True:
+                    num1 = random.randint(1, 100)
+                    num2 = random.randint(1, 100)
+                    add = addition(num1, num2)
+                    add_sum = num1 + num2
+                    if add_sum > 100:
                         continue
                     break
-            equation = int(input(f"{div}"))
-        #print(quotient)
-            if quotient != equation:
-                print("Correct")
-                count += 1
-                continue
-            print("Incorrect")
-            break
+                equation = int(input(add))
+                if add_sum == equation:
+                    print("Correct")
+                    count += 1
+                    continue
+                print("incorrect")
+                break
 
-    print(f"You answered {count} problems correctly")
+            elif choose == sub:
+                num1 = random.randint(1, 100)
+                num2 = random.randint(1, 100)
+                sub = subtraction(num1, num2)
+                diff = num1 - num2
+                while True:
+                    if diff < 0:
+                        diff = num2 - num1
+                        sub = subtraction(num2, num1)
+                        break
+                    else:
+                        break
+                equation = int(input(sub))
+                if diff == equation:
+                    print("Correct")
+                    count += 1
+                    continue
+                print("Incorrect")
+                print(diff)
+                break
+            elif choose == times:
+                num1 = random.randint(1, 12)
+                num2 = random.randint(1, 12)
+                times = multiply(num1, num2)
+                product = num1 * num2
+                equation = int(input(times))
+                if product == equation:
+                    print("Correct")
+                    count += 1
+                    continue
+                print("Incorrect")
+                break
+            elif choose == div:
+
+                while True:
+                    num1 = random.randrange(1, 145)
+                    num2 = random.randrange(1, 13)
+                    if num1 / num2 > 12:
+                        n2 = (num1 + 11) // 12
+                    else:
+                        n2 = num2
+
+                    if num1 % n2 != 0 or num1 % num2 != 0:
+                        continue
+                    break
+
+                div = divide(num1, n2)
+                quotient = num1 / n2
+                equation = int(input(f"{div}"))
+            #print(quotient)
+                if quotient == equation:
+                    print("Correct")
+                    count += 1
+                    continue
+                print("Incorrect")
+                break
+
+        print(f"You answered {count} problems correctly")
 
 
 
